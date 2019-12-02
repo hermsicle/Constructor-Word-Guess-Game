d849f625834e73ff59fce02077b953a01e4df2c8
Herman Liu <hermanliu95@gmail.com>
2019-12-01 18:32:33 -0800

    Have the constructors in letter and word

--- index.js
+++ index.js
@@ -6,5 +6,23 @@ const Word = require ('./word.js');
 //Create string of characrters
 let characters = 'abcdefghijklmnopqrstuvwxyz';
 
-//Create 
+//Create an array of possilbe words 
+let fastFoods = [
+    'McDonalds',
+    'Dominos',
+    'Wendys',
+    'Sonic',
+    'KFC',
+    'Subway',
+    'Quiznos',
+    'Chipotle',
+    'Whataburger',
+    'Arbys',
+    'Popeyes',
+    'Fatburger'
+]
 
+let randomIndex = Math.floor(Math.random()*fastFoods.length);
+let ranFastFood = fastFoods[randomIndex]
+
+console.log('Random Fast Food Joint: ' + ranFastFood)
--- letter.js
+++ letter.js
@@ -2,7 +2,7 @@
 //!Constructor should define : a string value to store the underlying character of the character
 //!A boolean value that stores whether that letter has been guessed yet.
 //!A function that returns the underlying character if the letter has been guessed, or a placeholder such as an underscore
-//!A function that takes a cahracter as an argument and checks it against the underlying character.
+//!A function that takes a character as an argument and checks it against the underlying character.
 //!letter.js should not require any other files. so we are using Module-exports in letter.js
 
 function Letter (value) {
